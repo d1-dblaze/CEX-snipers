a9b6815b66f2534aa5f7d9fee360e9bc1f49824f
d1-dblaze <adedayoajao7@gmail.com>
2023-08-05 15:54:07 +0100

    writing better function

--- mexc/mexc_monitor.py
+++ mexc/mexc_monitor.py
@@ -11,19 +11,33 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def getmylogger(name):
+    """
+    Create and configure a logger with file and console handlers.
+
+    Args:
+        name (str): The name of the logger.
+
+    Returns:
+        logging.Logger: The configured logger.
+
+    """
     formatter = logging.Formatter('[%(asctime)s] [%(levelname)s] [MODULE::%(module)s] [MESSAGE]:: %(message)s')
     
-    file_handler = logging.handlers.TimedRotatingFileHandler("../logs/mexc_monitor.log",when= "midnight")
+    # Configure the file handler for logging to a file with rotating file names
+    file_handler = logging.handlers.TimedRotatingFileHandler("../logs/mexc/mexc_monitor.log",when= "midnight")
     file_handler.setLevel(logging.INFO)
     file_handler.setFormatter(formatter)
+    
+    # Configure the console handler for logging to the console
     console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.INFO)
+    console_handler.setLevel(logging.DEBUG)
     console_handler.setFormatter(formatter) 
 
+    # Create a logger and add the file handler and console handler
     loggerHandle = logging.getLogger(name)
     loggerHandle.addHandler(file_handler)
     loggerHandle.addHandler(console_handler)
-    loggerHandle.setLevel(logging.DEBUG)
+    loggerHandle.setLevel(logging.INFO)
     
     return loggerHandle
 
@@ -44,26 +58,28 @@ def libraryConnect():
 
     return handle
  
-def return_unique_id():
-    return ''.join([each for each in str(uuid1()).split('-')])
- 
 def custom_market_sell_order (client,symbol,size):
     """
-    Function to place sell order on MEXC
+    Place a market sell order on mexc and handle errors with retries.
+
+    Args:
+        client: The CCXT client instance for mexc.
+        symbol (str): The trading symbol for the order.
+        size: The size or quantity to sell.
+
+    Returns:
+        dict or str: The order result if successful, or the encountered error.
+
     """
-    #This needs to return the price at which the order was opened
-    #alongside the order result.
-    """
-    This while loop is there to curb the 429 "Too many request" error which always lead
-    to loss of trades.
-    """
-    status = False          #status of the order
-    counter = 0             #Retry counter
+    # Retry counter and order status
+    counter = 0
+    status = False
+    
     #while the order status is false, keep trying to place the order
-    while status == False:
+    while not status:
         try: 
             time.sleep(1) #sleep for one sec
-            result = result = client.spotPrivatePostOrder({
+            result = client.spotPrivatePostOrder({
                 "symbol":symbol,
                 "side":"SELL",
                 "type":"MARKET",
@@ -72,73 +88,151 @@ def custom_market_sell_order (client,symbol,size):
             #change status to true once the order code execute without errors
             status = True
             return result
-        except Exception as e:
-            #returns the string representation of the execution error
-            err = repr(e)
-            #if the MEXC "Too many request" error is present, leave status as false and continue the loop
-            if '429000' in err:
-                counter += 1
-                logger.info("Encountered MEXC 'Too many Request' Error, Retrying order placement again "+ str(counter))
-                continue
-            #if the http "Too many request" error is present, leave status as false and continue the loop
-            elif '429' in err:
-                time.sleep(4) #sleep for 4 sec
-                logger.info(err)
-                counter += 1
-                logger.info("Encountered Http 'Too many Request' Error, Retrying order placement again "+ str(counter))
-                continue
-            #else, break from the loop and return the error encountered.
-            else:
+        except ccxt.RequestTimeout as e:
+            # Handle request timeout error
+            logger.info("Encountered request timeout error. Retrying order placement (Attempt {})".format(counter))
+            if counter == 3:
                 status = True
-                logger.info("Error encountered while placing an order - ".format(err))
-                return err
+                return e
+            counter += 1
+            time.sleep(1)  # Sleep for one second between retries
+        except ccxt.InsufficientFunds as e:                       
+            logger.info("Balance insufficient!")
+            status = True  # Set status to true 
+            return e
+        except ccxt.ExchangeError as e:
+            error_message = str(e)
+            logger.info("Encountered this Exchange error - {}".format(e))
+            if 'Too many requests' in error_message:
+                # Handle rate limit error (Too many requests)
+                logger.info("Encountered rate limit error. Retrying order placement (Attempt {})".format(counter))
+                if counter == 3:
+                    status = True
+                    break
+                counter += 1
+                time.sleep(1)  # Sleep for one second between retries
+            else:
+                # Handle other exchange errors
+                logger.info("Error encountered while placing an order: {}".format(error_message))
+                return error_message
+        except Exception as e:
+            # Handle general exceptions
+            error_message = str(e)
+            logger.info("Error encountered while placing an order: {}".format(error_message))
+            return error_message
+        
+def clean(account_balance, details, current_price, side, risk_percentage):
+    """
+    Clean and process data for order size calculation.
 
-def clean (accountBalance,current_price,side,riskP):
-    logger.info("======cleaning data =======")
+    Args:
+        account_balance (float): The account balance to consider for risk calculation.
+        details (dict): Details of the trading pair or instrument.
+        current_price (float): The current price of the asset.
+        side (str): The side of the trade, either "buy" or "sell".
+        risk_percentage (float): The risk percentage to consider for order size calculation.
+
+    Returns:
+        float: The cleaned and calculated order size.
+
+    """
+    logger.debug("Cleaning data")
+
     decimal.getcontext().rounding = decimal.ROUND_DOWN
-    #convert the risk% to float.
-    risk = float(riskP)
 
-    #if side is buy, calculate the size with the formula below
-    if side == "buy":    
-        size = decimal.Decimal(risk/100 * accountBalance) / decimal.Decimal(current_price)
-        #round down to the nearest whole number.
-        size_to_exchange = round(size)
-        #if side is sell, sell 100% of the asset.
-    elif side == 'sell':
-        size = decimal.Decimal(risk/100 * accountBalance) * decimal.Decimal(1)
-        size_to_exchange = round(size)
+    # Convert the risk percentage to float.
+    risk = float(risk_percentage)
+
+    # Extract the base increment value for order size calculation.
+    base_increment = details['baseAssetPrecision']
+
+    # convert decimal place to int.
+    decimal_places = int(base_increment)
+
+    logger.debug("The order size should be rounded to %d decimal places", decimal_places)
+
+    # Calculate the order size based on the side of the trade.
+    if side == "buy":
+        size = decimal.Decimal(risk / 100 * account_balance) / decimal.Decimal(current_price)
+    elif side == "sell":
+        size = decimal.Decimal(risk / 100 * account_balance)
+
+    # Round the order size to the specified decimal places.
+    size_to_exchange = round(size, decimal_places)
 
     return float(size_to_exchange)
 
 def getAccountBalance (client,currency):
-    logger.info ("======Retrieving account Details======")
-    acctInfo = client.spotPrivateGetAccount()
-    logger.info("acctInfo: {} ".format (acctInfo))
+    """
+    Retrieve the available balance of a specific currency in the account.
+
+    Args:
+        client: The client object for interacting with the exchange.
+        currency (str): The currency symbol to retrieve the balance for.
+
+    Returns:
+        float or str: The available balance of the currency as a float if it exists, or an error message as a string.
+    """
+    logger.info("Retrieving account details")
+    account_info = client.spotPrivateGetAccount()
+    logger.info("Account information: {}".format(account_info))
     
-    for asset in acctInfo["balances"]:
+    for asset in account_info["balances"]:
         if asset['asset'] == currency:
             availableBalance = asset['free']
-            #test
             logger.info('Available balance: {}'.format(availableBalance))
             return float(availableBalance)
-
-def getSymbolDetail (client, symbol):
-    symbolList = client.spotPublicGetExchangeInfo()['symbols']
-    for n in symbolList:
-        if n['symbol'] == symbol:
-            return n
+    
+    return 0
 
 def readTradeList():
-    with open("/root/snipeBot/mexc_trade_list.json", 'r') as trade_list:
-        data = json.load(trade_list)
-    return data
-
+    """
+    Reads and returns the data from the trade list file.
+    """
+    try:
+        with open("/root/snipeBot/mexc_trade_list.json", 'r') as trade_list:
+            data = json.load(trade_list)
+        return data
+    except FileNotFoundError:
+        logger.error("Trade list file not found.")
+        return None
+    except json.JSONDecodeError:
+        logger.error("Error decoding JSON data from the trade list file.")
+        return None
+    
 def rewrite(trade):
-    with open("/root/snipeBot/mexc_trade_list.json",'w') as trade_list:
-        data = json.load(trade_list)
-        data.remove(trade)
-        json.dump(data)
+    """
+    Rewrites the trade list file after removing the specified trade.
+    """
+    try:
+        with open("/root/snipeBot/mexc_trade_list.json", 'r') as trade_list:
+            data = json.load(trade_list)
+            data.remove(trade)
+        with open("/root/snipeBot/mexc_trade_list.json", 'w') as trade_list:
+            json.dump(data, trade_list)
+    except FileNotFoundError:
+        logger.error("Trade list file not found.")
+    except (json.JSONDecodeError, ValueError):
+        logger.error("Error decoding JSON data from the trade list file.")
+            
+def getSymbolDetail (client, symbol):
+    """
+    Retrieve details of a specific symbol from the exchange.
+
+    Args:
+        client: The client object for interacting with the exchange.
+        symbol (str): The symbol to retrieve details for.
+
+    Returns:
+        dict or None: Details of the symbol if found, None otherwise.
+    """
+    
+    symbolList = client.spotPublicGetExchangeInfo()['symbols']
+    for symbol_info in symbolList:
+        if symbol_info['symbol'] == symbol:
+            return symbol_info
+    
+    return None  # Symbol not found, return None
 
 def main():
     global client
